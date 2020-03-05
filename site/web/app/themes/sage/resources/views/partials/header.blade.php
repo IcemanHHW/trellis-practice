f818b5775be48a825655f3327deeebe6240f6510
IcemanHHW <maartenkampmeijer@hotmail.com>
2020-03-05 14:50:09 +0100

    trellis-practice 0.3.1
    - added classes to navbar
    
    (Bootstrap navbar icon doesnt work in Sage)

--- site/web/app/themes/sage/resources/views/partials/header.blade.php
+++ site/web/app/themes/sage/resources/views/partials/header.blade.php
@@ -1,10 +1,13 @@
 <header class="banner">
-  <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <nav class="nav-primary">
+    <nav class="nav-primary navbar fixed-top navbar-expand-lg navbar-dark bg-primary">
+      <a class="navbar-brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo">
       @if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu($primarymenu) !!}
       @endif
+      </div>
     </nav>
-  </div>
 </header>
