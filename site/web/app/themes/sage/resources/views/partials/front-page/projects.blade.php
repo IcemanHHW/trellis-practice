e6a79eebaeaea3dd2166ef09c3b0157a2aa4895f
Maarten <maartenkampmeijer@hotmail.com>
2020-04-09 15:27:09 +0200

    trellis practice 0.6
    - changed projects on frontpage to blog posts so they can be fetched with Frontify

--- site/web/app/themes/sage/resources/views/partials/front-page/projects.blade.php
+++ site/web/app/themes/sage/resources/views/partials/front-page/projects.blade.php
@@ -1,65 +1,57 @@
+@php
+
+$args = array(
+'post_type' => 'post',
+'posts_per_page' => 2,
+);
+
+$post_tags = get_the_tags();
+
+$loop = new WP_Query( $args );
+
+@endphp
 <div class="container">
   <h2>Portfolio</h2>
   <p class="h5">M'n laatste projecten</p>
   <div class="card-deck mt-3">
-    <div class="card shadow-sm">
-      <div class="card-header bg-transparent">
+    <?= while ( $loop->have_posts() ) : $loop->the_post(); ?>
+      <div class="card shadow-sm">
+        <div class="card-header bg-transparent">
           <div class="row">
-              <div class="col-md-auto">
-                <p class="h4"><?= $hp_title_1 ?></p>
-              </div>
-              <div class="col">
-                <span class="badge badge-light">WordPress</span>
-                <span class="badge badge-light">Custom thema</span>
-                <span class="badge badge-light">WP Plugins</span>
-                <span class="badge badge-light">PHP</span>
-                <span class="badge badge-light">Isotope</span>
-                <span class="badge badge-light">jQuery</span>
-              </div>
-          </div>
-      </div>
-      <img src="<?= $hp_image_1 ?>" class="card-img-top" alt="Pure Face Art">
-      <div class="card-body">
-        <p class="card-text"><?= $hp_text_1 ?></p>
-      </div>
-      <div class="card-footer bg-transparent">
-          <div class="row">
-              <div class="col text-center">
-                <a href="https://daimond.jobgroen.nl/" target="_blank" class="card-link">De website</a>
-              </div>
-              <div class="col text-center">
-                <a href="https://github.com/IcemanHHW/DFV" target="_blank" class="card-link">De code</a>
-              </div>
-          </div>
-      </div>
-    </div>
-    <div class="card shadow-sm">
-        <div class="card-header bg-transparent pb-4">
-            <div class="row">
-                <div class="col-md-auto">
-                  <p class="h4"><?= $hp_title_2 ?></p>
-                </div>
-                <div class="col">
-                  <span class="badge badge-light">WordPress</span>
-                  <span class="badge badge-light">WP Plugins</span>
-                  <span class="badge badge-light">PHP</span>
-                </div>
+            <div class="col-md-auto">
+              <p class="h4"><?= the_title(); ?></p>
             </div>
+            <div class="col">
+              @php
+
+                if ( $post_tags ):
+                  foreach( $post_tags as $tag ):
+
+                  @endphp
+                   <span class="badge badge-light"><?= $tag->name; ?></span> 
+                  @php
+
+                  endforeach
+                endif
+
+              @endphp
+            </div>
+          </div>
         </div>
-        <img src="<?= $hp_image_2 ?>" class="card-img-top" alt="Afvallen met Nederland">
+        <?= if( has_post_thumbnail() ): ?>
+          <img src="<?= the_post_thumbnail_url(); ?>" class="card-img-top" alt="<?= the_title(); ?>">
+        <?= endif;?>
         <div class="card-body">
-          <p class="card-text"><?= $hp_text_2 ?></p>
+          <p class="card-text"><?= the_excerpt(); ?></p>
         </div>
         <div class="card-footer bg-transparent">
-            <div class="row">
-                <div class="col text-center">
-                  <a href="https://afvallenmetnederland.nl/" target="_blank" class="card-link">De website</a>
-                </div>
-                <div class="col text-center">
-                  <a href="https://ma.kampmeijer.nl/stageblog/category/fusionlab/" target="_blank" class="card-link">Stageblog</a>
-                </div>
-            </div>
+          <a href="<?php the_permalink(); ?>" class="card-link">Lees meer</a>
         </div>
       </div>
+    @php
+      endwhile;
+
+      wp_reset_postdata();
+    @endphp
   </div>
 </div>
